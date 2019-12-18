c60d2c67a009aaf60b93e212b9af8ee4981dfd4e
Jeremy McWilliams <j2macwilliams@gmail.com>
2019-12-18 15:13:44 -0800

    Create Splash page. Frix error in NavBar.

--- client/src/components/NavBar.js
+++ client/src/components/NavBar.js
@@ -1,21 +1,26 @@
 import React from 'react';
 import { NavLink } from 'react-router-dom';
-import axios from 'axios';
 
 function NavBar() {
-	return;
-	<div>
-		<NavLink exact to='/' activeClassName='activeNavButton' className='navLink'>
-			Home
-		</NavLink>
-		<NavLink
-			to='/home-page'
-			activeClassName='activeNavButton'
-			className='navLink'
-		>
-			Users
-		</NavLink>
-	</div>;
+	return (
+		<div>
+			<NavLink
+				exact
+				to='/'
+				activeClassName='activeNavButton'
+				className='navLink'
+			>
+				Home
+			</NavLink>
+			<NavLink
+				to='/home-page'
+				activeClassName='activeNavButton'
+				className='navLink'
+			>
+				Users
+			</NavLink>
+		</div>
+	);
 }
 
 export default NavBar;
