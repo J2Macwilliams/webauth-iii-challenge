913bb6ff662652728f041c53343782eccee84494
Jeremy McWilliams <j2macwilliams@gmail.com>
2019-12-18 15:10:49 -0800

    Create NavLinks to respective pages.

--- client/src/components/NavBar.js
+++ client/src/components/NavBar.js
@@ -1,13 +1,21 @@
-import React from 'react'
-import {NavLink } from 'react-router-dom'
-import axios from 'axios'
+import React from 'react';
+import { NavLink } from 'react-router-dom';
+import axios from 'axios';
 
 function NavBar() {
-    return (
-        <div>
-            
-        </div>
-    )
+	return;
+	<div>
+		<NavLink exact to='/' activeClassName='activeNavButton' className='navLink'>
+			Home
+		</NavLink>
+		<NavLink
+			to='/home-page'
+			activeClassName='activeNavButton'
+			className='navLink'
+		>
+			Users
+		</NavLink>
+	</div>;
 }
 
-export default NavBar
+export default NavBar;
