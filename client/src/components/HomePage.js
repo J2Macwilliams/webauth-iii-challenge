3697edc0f94f2e3c7a195d45f2879144d81dae33
Jeremy McWilliams <j2macwilliams@gmail.com>
2019-12-18 15:31:57 -0800

    SetUp rough Homepage.

--- client/src/components/HomePage.js
+++ client/src/components/HomePage.js
@@ -1,19 +1,53 @@
 import React, { useState, useEffect } from 'react';
+import axios from 'axios';
+import {
+	Container,
+	Grid,
+	Card,
+	Typography,
+	makeStyles
+} from '@material-ui/core';
+
+const useStyles = makeStyles(theme => ({
+	root: {
+		flexGrow: 1
+	},
+	card: {
+		background: '',
+		border: '1px solid #'
+	},
+	type: {}
+}));
 
 function HomePage() {
-const [users , setUsers] = useState([])
+	const classes = useStyles();
+	const [users, setUsers] = useState([]);
 	useEffect(() => {
 		axios
-			.get('')
+			.get('http://localhost:5000/api/restricted/users')
 			.then(res => {
-				console.log(res);
+                console.log(res);
+                setUsers(res.data)
 			})
 			.catch(error => {
 				console.log(error);
 			});
 	}, []);
 
-	return <div></div>;
+	return (
+		<div>
+			<Container className={classes.root}>
+				{users.map(e => (
+					<Grid item xs={12} sm={6} md={3} >
+						<Card>
+							<Typography>{e.username}</Typography>
+							<Typography>{e.department}</Typography>
+						</Card>
+					</Grid>
+				))}
+			</Container>
+		</div>
+	);
 }
 
 export default HomePage;
