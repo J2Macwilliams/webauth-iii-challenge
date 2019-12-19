12db79d295fcf6141118d1870a63052a400acf63
Jeremy McWilliams <j2macwilliams@gmail.com>
2019-12-19 06:43:31 -0800

    Create Axios with auth.

--- middleware/restricted.js
+++ middleware/restricted.js
@@ -1,21 +1,21 @@
-const jwt = require("jsonwebtoken"); // installed this
+const jwt = require('jsonwebtoken'); // installed this
 
 module.exports = (req, res, next) => {
-  const { authorization } = req.headers;
+	const { authorization } = req.headers;
 
-  if (authorization) {
-    const secret = process.env.JWT_SECRET;
+	if (authorization) {
+		const secret = process.env.JWT_SECRET;
 
-    jwt.verify(authorization, secret, function(err, decodedToken) {
-      if (err) {
-        res.status(401).json({ message: "Invalid Token" });
-      } else {
-        req.token = decodedToken;
+		jwt.verify(authorization, secret, function(err, decodedToken) {
+			if (err) {
+				res.status(401).json({ message: 'Invalid Token' });
+			} else {
+				req.token = decodedToken;
 
-        next();
-      }
-    });
-  } else {
-    res.status(400).json({ message: "Please login and try again" });
-  }
+				next();
+			}
+		});
+	} else {
+		res.status(400).json({ message: 'Please login and try again' });
+	}
 };
